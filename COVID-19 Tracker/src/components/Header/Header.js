bb24ffe59ceb642ad5f51d97bec40144013cd891
SMS47 <43289216+SMS47@users.noreply.github.com>
2020-10-06 13:58:42 -0600

    Small Update to Remove Text for Next Sprint

--- COVID-19 Tracker/src/components/Header/Header.js
+++ COVID-19 Tracker/src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="Header">
       <h1>USA COVID-19 Tracker</h1>
-      <h2>Click on a State to view individual state data.</h2>
+      {/* <h2>Click on a State to view individual state data.</h2> */}
     </header>
   );
 };
